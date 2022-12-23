382dc958a70c661a9bcd0bad0ca4c9f1740b26bb
vishnu <vishnubhel@rediffmail.com>
2022-12-23 18:12:54 +0530

    secondpro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const name="Vishnu";
+//const currDate = new Date().toLocaleDateString();
+//const currTime = new Date().toLocaleTimeString();
+let currTime=new Date(2020,5,5,15);
+currTime = currTime.getHours();
+// const img1= " https,://picsum.photos/200/300";
+// const img2= " https://picsum.photos/200/300";
+// const img3= " https://picsum.photos/200/300";
+// const link="https://www.w3schools.com/";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let greetings=''; 
+const cssStyle={ };
+
+if(currTime<=12){
+greetings="Good Morning";
+cssStyle.color='blue';
+}
+else if(currTime>12&&currTime<18){
+greetings="Good Afternoon";
+cssStyle.color='orange';
+}
+else{
+    greetings="Good Night";
+    cssStyle.color='navyblue';
+}
+
+
+ReactDOM.render(
+    
+
+<h1>Hello Sir,<span style={cssStyle}>{greetings}</span></h1>
+//<p>todays date is { //currDate }</p>
+//<p>Current time is {//currTime}</p>
+//<img src={img1} alt="randomImage"/>
+//img src={img2} alt="randomImage"/>
+//<a href={link} target="_w3school">
+//<img src={img3} alt="randomImage" />
+//</a>
+
+
+, document.getElementById("root"));
